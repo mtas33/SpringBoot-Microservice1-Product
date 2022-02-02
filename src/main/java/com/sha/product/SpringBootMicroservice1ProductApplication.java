3dc1c7ba33f3ad3eaf0436cb232431e00ae400ed
mtas33 <mustafatas02@ogm.gov.tr>
2022-02-02 11:31:38 +0300

    initial commit

--- src/main/java/com/sha/product/SpringBootMicroservice1ProductApplication.java
+++ src/main/java/com/sha/product/SpringBootMicroservice1ProductApplication.java
@@ -3,11 +3,15 @@ package com.sha.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class SpringBootMicroservice1ProductApplication {
+@PropertySource("classpath:application-${spring.profiles.active.default}.properties")
+public class SpringBootMicroservice1ProductApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(SpringBootMicroservice1ProductApplication.class, args);
 	}
 
