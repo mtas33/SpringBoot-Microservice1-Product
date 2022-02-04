94ecf152828d37d915d9262d586f95e264a0d6d0
mtas33 <mustafatas02@ogm.gov.tr>
2022-02-04 14:00:33 +0300

    initialcommit

--- src/main/java/com/sha/product/model/entity/Product.java
+++ src/main/java/com/sha/product/model/entity/Product.java
@@ -31,5 +31,5 @@ public class Product extends Entity<Integer>
 
     @Temporal(TemporalType.DATE)
     @Column(nullable = false)
-    private Date created;
+    private Date created = new Date();
 }
